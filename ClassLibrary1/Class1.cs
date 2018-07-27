88afef5c05f3805794129bad284ecf98619c6f4e
stjeong <kevin13@chol.net>
2018-07-27 10:08:33 +0900

    added .NET Framework ClassLibrary sample

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class SampleType2
+    {
+        public void DoNothing(int n)
+        {
+            int n2 = n * 2;
+            Console.WriteLine(n2);
+        }
+    }
+}
